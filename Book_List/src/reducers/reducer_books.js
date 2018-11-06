14f84843d44369e85cd04bb2996a967549d5f708
gael <gael.russeil@hotmail.fr>
2018-11-06 18:49:06 +0100

    save

---
+++ Book_List/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JS : Good Parts', pages: 101},
+    { title: 'Harra Potter', pages: 48 },
+    { title: 'Dark Tower', pages: 98 },
+    { title: 'Eloquent Ruby', pages: 1 }
+  ]
+}
